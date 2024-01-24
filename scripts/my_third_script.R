d016cecfa11f82cc90593c3e642b1f992eb27c48
Eleonora Mastrorilli <13322255+Aron-github@users.noreply.github.com>
2024-01-24 10:06:31 +0100

    Yay, factors!

--- scripts/my_third_script.R
+++ scripts/my_third_script.R
@@ -48,9 +48,58 @@ surveys[nrow(surveys), ]
 
 surveys[ nrow(surveys)/2, ]
 
-my_list <- list(names = c("Nora", "Lisanna", "Francesco"),
-                money = c(1, 6, 7, 3, 5, 8))
-my_list[[1]]
-my_list$names
+# my_list <- list(names = c("Nora", "Lisanna", "Francesco"),
+#                 money = c(1, 6, 7, 3, 5, 8))
+# my_list[[1]]
+# my_list$names
 
 surveys[[3]]
+
+
+# Factors
+str(surveys)
+surveys$sex <- factor(surveys$sex)
+
+levels(surveys$sex)
+nlevels(surveys$sex)
+
+sex <- factor(c("male", "female", "female", "male"))
+sex <- factor(sex, levels = c("male", "female"))
+
+# Challenge
+surveys$taxa <- factor(surveys$taxa)
+surveys$genus <- factor(surveys$genus)
+
+levels(surveys$taxa)
+nlevels(surveys$taxa)
+nlevels(surveys$genus)
+sum(surveys$taxa == "Rabbit")
+summary(surveys)
+
+# convert factors
+as.character(sex)
+
+year_fct <- factor(c(1990, 1983, 1977, 1997))
+as.numeric(year_fct)
+
+as.numeric(as.character(year_fct))
+as.numeric(levels(year_fct))[year_fct]
+
+# Renaming factors
+plot(surveys$sex)
+summary(surveys$sex)
+sex <- surveys$sex
+levels(sex)
+sex <- addNA(sex)
+levels(sex)
+
+levels(sex)[3] <- "undetermined"
+levels(sex)
+sex
+
+plot(sex)
+
+levels(sex)[1:2] <- c("female", "male")
+plot(sex)
+sex <- factor(sex, levels = c("undetermined", "female", "male"))
+plot(sex)
